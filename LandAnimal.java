385af6f9689c6f5ff9707ec54e7a83e1326e1548
Ubrizze <132262101+Ubrizze@users.noreply.github.com>
2024-06-11 01:49:58 +0100

    Land animal code

--- LandAnimal.java
+++ LandAnimal.java
@@ -1,11 +1,11 @@
 public class LandAnimal extends Animal {
     @Override
     String move() {
-        return "Swings on tree";
+        return "Climbs rocks";
     }
     public static void main(String[] args) {
         LandAnimal lion = new LandAnimal();
-        lion.name = "Leo";
+        lion.name = "Simba";
         LandAnimal monkey = new LandAnimal();
         System.out.println(lion.move());
         System.out.println(monkey.move());
