4eef94f5da1081b4bdd8701c5812c2bd722596c4
Ubrizze <132262101+Ubrizze@users.noreply.github.com>
2024-06-11 02:23:04 +0100

    Students code

--- User/Student.java
+++ User/Student.java
@@ -1,8 +1,8 @@
 package User;
 
 public class Student extends User {
-    String matric_no = "bhu/22/04/05/0054";
-    String admission_no = "1789237958103";
+    String matric_no = "bhu/22/04/05/0134";
+    String admission_no = "134";
     String registerCourse() {
         if (matric_no.equals("") || admission_no.equals("")) {
             return "You cannot register at this moment";
@@ -14,7 +14,7 @@ public class Student extends User {
         if (matric_no.equals("") || admission_no.equals("")) {
             return "Cannot check your result. Are you even a student?";
         } else {
-            return "You got 6As and 1B";
+            return "You got 5A, 2C and 3B";
         }
     }    
 }
