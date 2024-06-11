f9402dc0146105276721d10bb084695036583579
Ubrizze <132262101+Ubrizze@users.noreply.github.com>
2024-06-11 10:15:51 +0100

    Update Swap.java

--- Algos/Swap.java
+++ Algos/Swap.java
@@ -2,7 +2,7 @@ public class Swap {
     public static void main(String[] args) {
         int number = 48;
         int anotherNumber = 112;
-        int lastNum = number;
+        int temp = number;
         number = anotherNumber;
         temp = lastNum;
         System.out.println(temp);
