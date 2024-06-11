52f92412948a7465e8ccd5a8e3e1cee3cefc4dda
Ubrizze <132262101+Ubrizze@users.noreply.github.com>
2024-06-11 10:09:30 +0100

    Fib sequence

--- Algos/Fib.java
+++ Algos/Fib.java
@@ -1,20 +1,4 @@
-package DSA.Arrays.Algos;
-
 public class Fib {
-    // public static void main(String[] args) {
-    //     Fib print = new Fib();
-    //     print.printFibo(5);
-    // }
-    // void printFibo(int n) {
-    //     int a = 0;
-    //     int b = 1;
-    //     for (int i = 1; i < n; i++) {
-    //         System.out.println(a);
-    //         int next = a + b;
-    //         a = b;
-    //         b = next;
-    //     }
-    // }
     public static void main(String[] args) {
         int first = 0, second = 1, next;
         System.out.println(first);
